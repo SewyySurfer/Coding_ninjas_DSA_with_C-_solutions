38eea3cd4d25bf72d39cb0819e93728b91c150e2
JatinRauthan <jatinrauthan0510@gmail.com>
2022-12-01 23:31:24 +0530

    Update stringToInteger.cpp

--- stringToInteger.cpp
+++ stringToInteger.cpp
@@ -1,7 +1,31 @@
-#include<bits/stdc++.h>
+/*
+String to Integer
+Send Feedback
+Write a recursive function to convert a given string into the number it represents. That is input will be a numeric string that contains only numbers, you need to convert the string into corresponding integer and return the answer.
+Input format :
+Numeric string S (string, Eg. "1234")
+Output format :
+Corresponding integer N (int, Eg. 1234)
+Constraints :
+0 < |S| <= 9
+where |S| represents length of string S.
+Sample Input 1 :
+00001231
+Sample Output 1 :
+1231
+Sample Input 2 :
+12567
+Sample Output 2 :
+12567
+*/
+
+
+
 
 //Solution1: Common sense with recursion
 /*
+#include<bits/stdc++.h>
+
 int stringToNumber(char input[]) {
     // Write your code here
     static int sum = 0;
@@ -15,6 +39,8 @@ int stringToNumber(char input[]) {
 
 
 // Solution 2: Head recursion 
+#include<bits/stdc++.h>
+
  int stringToNumber(char input[]) {
 	if(strlen(input) == 0) return 0;
     int a = input[0] - 48;
