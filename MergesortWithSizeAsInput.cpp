d0702702a33e2cd2243c2130d5368a699c29137c
JatinRauthan <jatinrauthan0510@gmail.com>
2023-02-13 14:46:28 +0530

    Update MergesortWithSizeAsInput.cpp

--- MergesortWithSizeAsInput.cpp
+++ MergesortWithSizeAsInput.cpp
@@ -15,20 +15,24 @@ else
   }
   while( i < size1)
   {
-    output[k] = input1[i]; k++; i++; 
+    output[k] = input1[i];
+    k++;
+    i++; 
   } 
   while(j < size2)
   {
-    output[k] = input2[j]; j++; k++; } 
+    output[k] = input2[j]; 
+    j++;
+    k++; } 
 } 
 void mergeSort(int input[], int size)
 { 
   if(size <= 1)
-  { return; } 
+  { return; 
+  } 
   int mid = size / 2; 
   int part1[500], part2[500]; 
-  int size1 = mid, 
-  size2 = size - mid; 
+  int size1 = mid, size2 = size - mid; 
   for(int i = 0; i < mid; i++)
   { 
     part1[i] = input[i]; 
@@ -36,6 +40,7 @@ void mergeSort(int input[], int size)
   int k = 0; 
   for(int i = mid ; i < size; i++)
   {
-    part2[k] = input[i]; k++; 
+    part2[k] = input[i];
+    k++; 
   } 
                                                                                mergeSort(part1, size1); mergeSort(part2, size2); merge(part1, size1, part2, size2, input); }
